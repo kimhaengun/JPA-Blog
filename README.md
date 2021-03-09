54fcf9b7debb375cdec0536cf9c3415dee1fc64e
kimhaengun <https://github.com/kimhaengun>
2021-03-09 14:01:59 +0900

    blog 세팅

---
+++ README.md
@@ -0,0 +1,14 @@
+# 스프링부트 블로그 프로젝트 V2
+
+## 의존성
+- Spring web
+
+
+## DB 설정
+
+```sql
+create user 'pos'@'%' identified by 'pos1234';
+GRANT ALL PRIVILEGES ON *.* TO 'pos'@'%';
+create database pos;
+```
+
---
+++ src/main/java/com/cos/blog/web/UserController.java
@@ -0,0 +1,12 @@
+package com.cos.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	@GetMapping({"","/"})
+	public String home() {
+		return "index";
+	}
+}
